943eb4c0dacef890f0d1f4bc71ab387ae330c0d5
Dellasvasileios <dellas.vasileios2@gmail.com>
2020-03-17 16:02:16 +0200

    register user

--- app/src/main/java/com/example/shoppinglist/LogInActivity.java
+++ app/src/main/java/com/example/shoppinglist/LogInActivity.java
@@ -2,21 +2,68 @@ package com.example.shoppinglist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import javax.security.auth.login.LoginException;
 
 public class LogInActivity extends AppCompatActivity {
 
     TextView registerTextView;
+    EditText editTextUserName,editTextPassword;
+    Button loginButton;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        registerTextView = findViewById(R.id.register);
+
+        registerTextView = (TextView)findViewById(R.id.register);
+
+        loginButton = (Button) findViewById(R.id.submit);
+        editTextUserName = (EditText) findViewById(R.id.editText_username);
+        editTextPassword = (EditText) findViewById(R.id.editText_password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editTextUserName.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(LogInActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else{
+                            Toast.makeText(LogInActivity.this, "Credentials are not valid", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    });
+            }
+        });
 
         registerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
